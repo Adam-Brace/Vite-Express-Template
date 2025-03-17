1e5c86f230cb1b4a233fa64af11d3943025c1da2
Adam-Brace <adambrace03@gmail.com>
2025-03-17 11:38:20 -0400

    kenex fix

--- server/knexfile.js
+++ server/knexfile.js
@@ -4,36 +4,4 @@ module.exports = {
 		client: "pg",
 		connection: `postgres://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@localhost/${process.env.DATABASE_PORT}`,
 	},
-
-	staging: {
-		client: "postgresql",
-		connection: {
-			database: "my_db",
-			user: "username",
-			password: "password",
-		},
-		pool: {
-			min: 2,
-			max: 10,
-		},
-		migrations: {
-			tableName: "knex_migrations",
-		},
-	},
-
-	production: {
-		client: "postgresql",
-		connection: {
-			database: "my_db",
-			user: "username",
-			password: "password",
-		},
-		pool: {
-			min: 2,
-			max: 10,
-		},
-		migrations: {
-			tableName: "knex_migrations",
-		},
-	},
 };
--- server/src/index.js
+++ server/src/index.js
@@ -2,10 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT;
+const cors = require("cors");
+const kenex = require("knex")(require("./knexfile")["development"]);
 
 app.get("/", (req, res) => {
 	res.send("Hello World!");
 });
+
+app.use(cors());
 
 const server = app.listen(PORT, () => {
 	console.log(`App listening at http://localhost:${PORT}`);
