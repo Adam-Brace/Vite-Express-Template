082c81a6912666cf58dc3c17c417b9d8a0d977da
Adam-Brace <adambrace03@gmail.com>
2025-04-14 13:09:44 -0400

    Added flexible server ports to the front end

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,16 @@
+import { useState, useEffect } from "react";
 import "./App.css";
+const API_URL = import.meta.env.VITE_API_URL; // This is the API URL from the server ex: http://localhost:3001
 
 function App() {
-	return <>Hello World</>;
+	const [data, setData] = useState(null);
+	useEffect(() => {
+		fetch(`${API_URL}`)
+			.then((response) => response)
+			.then((data) => setData(data));
+	}, []);
+
+	return data ? "Api is working" : "Api is not working";
 }
 
 export default App;
--- client/vite.config.js
+++ client/vite.config.js
@@ -6,16 +6,23 @@ import dotenv from "dotenv";
 dotenv.config({ path: "/app_data/.env" });
 
 var PORT = process.env.CLIENT_PORT;
+var SERVER_PORT = process.env.SERVER_PORT;
 let open = false;
 
 if (!PORT) {
 	dotenv.config({ path: path.resolve(__dirname, "../.env") });
 	PORT = process.env.CLIENT_PORT;
+	SERVER_PORT = process.env.SERVER_PORT;
 	open = true;
 }
 
 export default defineConfig({
 	plugins: [react()],
+	define: {
+		"import.meta.env.VITE_API_URL": JSON.stringify(
+			`http://localhost:${SERVER_PORT}`
+		),
+	},
 	server: {
 		port: PORT,
 		host: true,
